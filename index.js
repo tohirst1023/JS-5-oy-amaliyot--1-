d187d09fe4f075e275f94ae195cb021e16393e77
Tohir Arapjonov <toxirarapjonov@gmail.com>
2024-06-27 19:16:53 +0500

    tdffdb

---
+++ index.js
@@ -0,0 +1,5 @@
+let color =  prompt("Textni rangini kiriting")
+let backraund = prompt(" bg-color kiritng")
+let body = document.querySelector("body");
+
+body.style = `background-color:${color}; color: ${backraund}`;
